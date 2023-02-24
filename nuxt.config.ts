566a40996b725842f90fa4a275b3e8e4bd3c6534
Jackson Meyer <jmeyerer18@gmail.com>
2023-02-24 09:01:57 -0600

    Add initial version of the project (still has some weird GraphQL bugs)

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  target: 'static',
+  build: {
+
+  },
+  env: {
+    strapiBaseUri: process.env.API_URL || "http://localhost:1337"
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/apollo',
+    '@nuxt/image-edge',
+  ],
+  buildModules: [
+
+  ],
+  image: {
+
+  },
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: process.env.BACKEND_URL || "http://localhost:1337/graphql"
+      }
+    },
+  },
+  head: {
+    css: [
+        "~/assets/style/main.css"
+    ],
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        
+      }
+    },
+  },
+  plugins: [
+    require("daisyui"),
+  ],
+  daisyui: {
+    styled: true,
+    // themes: [
+    //   {
+    //     main: {
+    //       primary: '',
+    //       secondary: '',
+    //       accent: '',
+    //       neutral: '',
+    //       "base-100": '',
+    //       warning: '',
+    //       error: '',
+    //     },
+    //   },
+    // ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+  },
+}
